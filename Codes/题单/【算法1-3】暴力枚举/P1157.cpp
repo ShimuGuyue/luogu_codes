634d507ab1b2216568c07409334ae994f7e22066
ShimuGuyue <2147539031@qq.com>
2024-11-03 14:01:43 +0800

    打表改为Dfs

--- Codes/题单/【算法1-3】暴力枚举/P1157.cpp
+++ Codes/题单/【算法1-3】暴力枚举/P1157.cpp
@@ -1,251 +1,43 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
+
+int n, r;
+std::vector<std::vector<int>> anss;
+
+void Dfs(int index, std::vector<int> vi);
 
 int main(void)
 {
-    int nn, rr;
-    std::cin >> nn >> rr;
-    if (rr == 0)
-        return 0;
-    for (int a(1); a <= nn; ++a)
+    std::cin >> n >> r;
+    std::vector<int> temp;
+    Dfs(1, temp);
+    int m(anss.size());
+    for (int i(1); i <= m; ++i)
     {
-        if (rr == 1)
+        for (int j(0); j < r; ++j)
         {
-            std::cout << 
-            std::setw(3) << a << 
-            std::endl;
-            continue;
+            std::cout << std::setw(3) << anss[m-i][j];
         }
-    for (int b(a + 1); b <= nn; ++b)
-    {
-        if (rr == 2)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << 
-            std::endl;
-            continue;
-        }
-    for (int c(b + 1); c <= nn; ++c)
-    {
-        if (rr == 3)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << 
-            std::endl;
-            continue;
-        }
-    for (int d(c + 1); d <= nn; ++d)
-    {
-        if (rr == 4)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d << 
-            std::endl;
-            continue;
-        }
-    for (int e(d + 1); e <= nn; ++e)
-    {
-        if (rr == 5)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e <<
-            std::endl;
-            continue;
-        }
-    for (int f(e + 1); f <= nn; ++f)
-    {
-        if (rr == 6)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f <<
-            std::endl;
-            continue;
-        }
-    for (int g(f + 1); g <= nn; ++g)
-    {
-        if (rr == 7)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g <<
-            std::endl;
-            continue;
-        }
-    for (int h(g + 1); h <= nn; ++h)
-    {
-        if (rr == 8)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::endl;
-            continue;
-        }
-    for (int i(h + 1); i <= nn; ++i)
-    {
-        if (rr == 9)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i <<
-            std::endl;
-            continue;
-        }
-    for (int j(i + 1); j <= nn; ++j)
-    {
-        if (rr == 10)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j <<
-            std::endl;
-            continue;
-        }
-    for (int k(j + 1); k <= nn; ++k)
-    {
-        if (rr == 11)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k <<
-            std::endl;
-            continue;
-        }
-    for (int l(k + 1); l <= nn; ++l)
-    {
-        if (rr == 12)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::endl;
-            continue;
-        }
-    for (int m(l + 1); m <= nn; ++m)
-    {
-        if (rr == 13)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m <<
-            std::endl;
-            continue;
-        }
-    for (int n(m + 1); n <= nn; ++n)
-    {
-        if (rr == 14)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m << std::setw(3) << n <<
-            std::endl;
-            continue;
-        }
-    for (int o(n + 1); o <= nn; ++o)
-    {
-        if (rr == 15)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m << std::setw(3) << n << std::setw(3) << o <<
-            std::endl;
-            continue;
-        }
-    for (int p(o + 1); p <= nn; ++p)
-    {
-        if (rr == 16)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m << std::setw(3) << n << std::setw(3) << o << std::setw(3) << p <<
-            std::endl;
-            continue;
-        }
-    for (int q(p + 1); q <= nn; ++q)
-    {
-        if (rr == 17)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m << std::setw(3) << n << std::setw(3) << o << std::setw(3) << p <<
-            std::setw(3) << q <<
-            std::endl;
-            continue;
-        }
-    for (int r(q + 1); r <= nn; ++r)
-    {
-        if (rr == 18)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m << std::setw(3) << n << std::setw(3) << o << std::setw(3) << p <<
-            std::setw(3) << q << std::setw(3) << r <<
-            std::endl;
-            continue;
-        }
-    for (int s(r + 1); s <= nn; ++s)
-    {
-        if (rr == 19)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m << std::setw(3) << n << std::setw(3) << o << std::setw(3) << p <<
-            std::setw(3) << q << std::setw(3) << r << std::setw(3) << s <<
-            std::endl;
-            continue;
-        }
-    for (int t(s + 1); t <= nn; ++t)
-    {
-        if (rr == 20)
-        {
-            std::cout << 
-            std::setw(3) << a << std::setw(3) << b << std::setw(3) << c << std::setw(3) << d <<
-            std::setw(3) << e << std::setw(3) << f << std::setw(3) << g << std::setw(3) << h <<
-            std::setw(3) << i << std::setw(3) << j << std::setw(3) << k << std::setw(3) << l <<
-            std::setw(3) << m << std::setw(3) << n << std::setw(3) << o << std::setw(3) << p <<
-            std::setw(3) << q << std::setw(3) << r << std::setw(3) << s << std::setw(3) << t <<
-            std::endl;
-            continue;
-        }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
+        std::cout << std::endl;
     }
     return 0;
+}
+
+void Dfs(int index, std::vector<int> vi)
+{
+    if (vi.size() == r)
+    {
+        anss.push_back(vi);
+        return;
+    }
+
+    if (index > n)
+    {
+        return;
+    }
+
+    Dfs(index + 1, vi);
+    vi.push_back(index);
+    Dfs(index + 1, vi);
 }
