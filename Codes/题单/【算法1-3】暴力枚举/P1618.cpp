e6841fbb111334f88748b48b9ae610d24ff21700
ShimuGuyue <2147539031@qq.com>
2024-11-03 14:12:48 +0800

    循环嵌套改为库函数调用

--- Codes/题单/【算法1-3】暴力枚举/P1618.cpp
+++ Codes/题单/【算法1-3】暴力枚举/P1618.cpp
@@ -1,62 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main(void)
 {
-    int x, y, z;
-    std::cin >> x >> y >> z;
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    std::vector<int> nums(10);
+    for (int i(1); i < 10; ++i)
+    {
+        nums[i] = i;
+    }
     bool ok(false);
-    for (int a(1); a <= 9; ++a)
+    do
     {
-    for (int b(1); b <= 9; ++b)
-    {
-    if (b == a)
-        continue;
-    for (int c(1); c <= 9; ++c)
-    {
-    if (c == a || c == b)
-        continue;
-    for (int d(1); d <= 9; ++d)
-    {
-    if (d == a || d == b || d == c)
-        continue;
-    for (int e(1); e <= 9; ++e)
-    {
-    if (e == a || e == b || e == c || e == d)
-        continue;
-    for (int f(1); f <= 9; ++f)
-    {
-    if (f == a || f == b || f == c || f == d || f == e)
-        continue;
-    for (int g(1); g <= 9; ++g)
-    {
-    if (g == a || g == b || g == c || g == d || g == e || g == f)
-        continue;
-    for (int h(1); h <= 9; ++h)
-    {
-    if (h == a || h == b || h == c || h == d || h == e || h == f || h == g)
-        continue;
-    for (int i(1); i <= 9; ++i)
-    {
-    if (i == a || i == b || i == c || i == d || i == e || i == f || i == g || i == h)
-        continue;
-    int flag1(a * 100 + b * 10 + c);
-    int flag2(d * 100 + e * 10 + f);
-    int flag3(g * 100 + h * 10 + i);
-    if (flag1 * y == flag2 * x && flag1 * z == flag3 * x && flag2 * z == flag3 * y)
-    {
-        ok = true;
-        std::cout << flag1 << " " << flag2 << " " << flag3 << std::endl;
+        int x(nums[1] * 100 + nums[2] * 10 + nums[3]);
+        int y(nums[4] * 100 + nums[5] * 10 + nums[6]);
+        int z(nums[7] * 100 + nums[8] * 10 + nums[9]);
+        if (x * b == y * a && x * c == z * a && y * c == z * b)
+        {
+            std::cout << x << " " << y << " " << z << std::endl;
+            ok = true;
+        }
     }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
-    }
+    while (next_permutation(nums.begin() + 1, nums.end()));
     if (!ok)
+    {
         std::cout << "No!!!" << std::endl;
-    return 0;                           
+    }
+    return 0;
 }
